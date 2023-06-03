cefe449b3943d62e96964f2a413e5c2ef1ad4fc8
toonor360 <omizmez@gmail.com>
2023-06-03 17:06:24 +0300

    connect firebase and users

---
+++ src/users/users.service.ts
@@ -0,0 +1,40 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserDocument } from './entities/users.document';
+import { CollectionReference } from '@google-cloud/firestore';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @Inject(UserDocument.collectionName)
+    private usersCollection: CollectionReference<UserDocument>,
+  ) {}
+
+  async create({ email, name }: UserDocument) {
+    const docRef = await this.usersCollection.add({ email, name });
+
+    return (await docRef.get()).data();
+  }
+
+  async findAll() {
+    const snapshot = await this.usersCollection.get();
+
+    const users: UserDocument[] = [];
+    snapshot.forEach((doc) => users.push(doc.data()));
+
+    return users;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
